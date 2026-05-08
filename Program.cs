23ff99b2a679e8d5bc06a1e0f651b02afa6d9858
yassine9812 <yassine50.zayet@gmail.com>
2026-05-08 18:56:43 +0100

    ADD Dictionary+Stack

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@
             books.Remove(removeBook);
             Console.WriteLine("Book removed successfully.");
             Console.WriteLine("-----------------------");
-            Console.WriteLine("The book removed is: "+removeBook.Title);
+            Console.WriteLine("The book removed is: " + removeBook.Title);
             Console.WriteLine("-----------------------");
             Console.WriteLine("Books in the collection after removal:");
             foreach (Book book in books)
@@ -53,11 +53,69 @@
         }
 
         Console.WriteLine("Tri books by price:");
-        List <Book> sortedBooks= books.OrderBy(b=>b.Price).ToList();
-        foreach(Book book in sortedBooks)
+        List<Book> sortedBooks = books.OrderBy(b => b.Price).ToList();
+        foreach (Book book in sortedBooks)
         {
             book.Display();
             Console.WriteLine("-----------------------");
         }
+
+        Console.WriteLine("=== Dictionary ===");
+
+        Dictionary<int, Book> bookDictionary = new Dictionary<int, Book>();
+        foreach (Book book in books)
+        {
+            bookDictionary.Add(book.Id, book);
+        }
+        Console.WriteLine("Enter the id of the book to search:");
+        int searchId = int.Parse(Console.ReadLine());
+        if (bookDictionary.TryGetValue(searchId, out Book foundBookById))
+        {
+            Console.WriteLine("Book found");
+            foundBookById.Display();
+        }
+        else
+        {
+            Console.WriteLine("Book not found.");
+        }
+
+        Console.WriteLine("Parcours Dictionary:");
+        foreach (KeyValuePair<int, Book> item in bookDictionary)
+        {
+            Console.WriteLine("Key: " + item.Key);
+            item.Value.Display();
+            Console.WriteLine("-----------------------");
+        }
+
+        Console.WriteLine("Utilsation of Stack:");
+        Stack<Book> bookStack = new Stack<Book>();
+        foreach (Book book in books)
+        {
+            bookStack.Push(book);
+        }
+        Console.WriteLine("Books in the stack:");
+        while (bookStack.Count > 0)
+        {
+            Book book = bookStack.Pop();
+            book.Display();
+            Console.WriteLine("-----------------------");
+        }
+
+        foreach (Book book in books)
+        {
+            bookStack.Push(book);
+        }
+
+        Console.WriteLine("Last book added to the stack");
+        if (bookStack.Count > 0)
+        {
+            Book lastBook = bookStack.Peek();
+            lastBook.Display();
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty.");
+        }
+
     }
 }
